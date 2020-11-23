04649f739687d8991f4be7f636bca3c80be44d88
qianjun <qjhj8ftn@gmail.com>
2020-11-23 17:35:57 +0800

    feat: add user

--- lib/screens/add_user/add_user.screen.dart
+++ lib/screens/add_user/add_user.screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class AddUserScreen extends StatefulWidget {
   @override
@@ -12,6 +13,24 @@ class _AddUserScreenState extends State<AddUserScreen> {
   final _nameController = TextEditingController();
   final _rocketNameController = TextEditingController();
   final _twitterController = TextEditingController();
+  String insertUser() {
+    return """
+      mutation insertUser(\$name: String!, \$rocket: String!, \$twitter: String!) {
+        insert_users(objects: {
+          name: \$name,
+          rocket: \$rocket,
+          twitter: \$twitter,
+        }) {
+          returning {
+            id
+            name
+            twitter
+            rocket
+          }
+        }
+      }
+    """;
+  }
 
   bool _isSaving = false;
 
@@ -42,105 +61,120 @@ class _AddUserScreenState extends State<AddUserScreen> {
                 ),
               ],
             ),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 12),
-                  TextFormField(
-                    controller: _nameController,
-                    decoration: new InputDecoration(
-                      labelText: "Name",
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(),
-                      ),
-                    ),
-                    validator: (v) {
-                      if (v.length == 0) {
-                        return "Name cannot be empty";
-                      } else {
-                        return null;
-                      }
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  SizedBox(height: 24),
-                  TextFormField(
-                    controller: _rocketNameController,
-                    decoration: InputDecoration(
-                      labelText: "Rocket name",
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(),
-                      ),
-                    ),
-                    validator: (v) {
-                      if (v.length == 0) {
-                        return "Rocket name cannot be empty";
-                      } else {
-                        return null;
-                      }
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  SizedBox(height: 24),
-                  TextFormField(
-                    controller: _twitterController,
-                    decoration: InputDecoration(
-                      labelText: "Twitter handle",
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(),
-                      ),
-                    ),
-                    validator: (v) {
-                      if (v.length == 0) {
-                        return "Twitter cannot be empty";
-                      } else {
-                        return null;
-                      }
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  SizedBox(height: 24),
-                  _isSaving
-                      ? SizedBox(
-                          height: 20,
-                          width: 20,
-                          child: CircularProgressIndicator(
-                            strokeWidth: 3,
+            child: Mutation(
+                options: MutationOptions(
+                  documentNode: gql(insertUser()),
+                  fetchPolicy: FetchPolicy.noCache,
+                  onCompleted: (data) => Navigator.pop(context, data != null),
+                ),
+                builder: (
+                  RunMutation runMutation,
+                  QueryResult result,
+                ) {
+                  return Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 12),
+                        TextFormField(
+                          controller: _nameController,
+                          decoration: new InputDecoration(
+                            labelText: "Name",
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(),
+                            ),
                           ),
-                        )
-                      : FlatButton(
-                          onPressed: () {
-                            if (_formKey.currentState.validate()) {
-                              setState(() {
-                                _isSaving = true;
-                              });
-
-                              // TODO: Add mutation here
+                          validator: (v) {
+                            if (v.length == 0) {
+                              return "Name cannot be empty";
+                            } else {
+                              return null;
                             }
                           },
-                          color: Color(0xFF005288),
-                          padding: const EdgeInsets.all(0),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 36,
-                              vertical: 12,
-                            ),
-                            child: Text(
-                              "Save",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                              ),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        SizedBox(height: 24),
+                        TextFormField(
+                          controller: _rocketNameController,
+                          decoration: InputDecoration(
+                            labelText: "Rocket name",
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(),
                             ),
                           ),
+                          validator: (v) {
+                            if (v.length == 0) {
+                              return "Rocket name cannot be empty";
+                            } else {
+                              return null;
+                            }
+                          },
+                          keyboardType: TextInputType.emailAddress,
                         ),
-                ],
-              ),
-            ),
+                        SizedBox(height: 24),
+                        TextFormField(
+                          controller: _twitterController,
+                          decoration: InputDecoration(
+                            labelText: "Twitter handle",
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(),
+                            ),
+                          ),
+                          validator: (v) {
+                            if (v.length == 0) {
+                              return "Twitter cannot be empty";
+                            } else {
+                              return null;
+                            }
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        SizedBox(height: 24),
+                        _isSaving
+                            ? SizedBox(
+                                height: 20,
+                                width: 20,
+                                child: CircularProgressIndicator(
+                                  strokeWidth: 3,
+                                ),
+                              )
+                            : FlatButton(
+                                onPressed: () {
+                                  if (_formKey.currentState.validate()) {
+                                    setState(() {
+                                      _isSaving = true;
+                                    });
+
+                                    runMutation({
+                                      'name': _nameController.text,
+                                      'rocket': _rocketNameController.text,
+                                      'twitter': _twitterController.text,
+                                    });
+                                  }
+                                },
+                                color: Color(0xFF005288),
+                                padding: const EdgeInsets.all(0),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 36,
+                                    vertical: 12,
+                                  ),
+                                  child: Text(
+                                    "Save",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                      ],
+                    ),
+                  );
+                }),
           ),
         ),
       ),
